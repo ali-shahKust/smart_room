e567ff594f9d6cefa3681636935f2c6eb944d331
ali-shahKust <syed.ali.shah3938@gmail.com>
2021-05-31 13:16:51 +0500

    nuemorphic data added

--- lib/screens/insight_explained.dart
+++ lib/screens/insight_explained.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:smart_room/utils/apptheme.dart';
@@ -53,38 +54,62 @@ class _Insight_ExplainedState extends State<Insight_Explained>with FakeChartSeri
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                CircularPercentIndicator(
-                  radius: 100.0,
-                  lineWidth: 10.0,
-                  animation: true,
-                  progressColor: Apptheme.primary,
-                  percent: 0.7,
-                  center: new Text(
-                    "70.0%",
-                    style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 11.0),
+                Neumorphic(
+                  style: NeumorphicStyle(
+                      shape: NeumorphicShape.concave,
+                      boxShape: NeumorphicBoxShape.circle(),
+                      depth: 4,
+                      lightSource: LightSource.topLeft,
+                      color: Apptheme.background
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: CircularPercentIndicator(
+                      radius: 100.0,
+                      lineWidth: 10.0,
+                      animation: true,
+                      progressColor: Apptheme.primary,
+                      percent: 0.7,
+                      center: new Text(
+                        "70.0%",
+                        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 11.0),
+                      ),
+                    ),
                   ),
                 ),
                 SizedBox(height: 15,),
 
                 Text("Less Instagram use might improve your sleep",style: iUtills.boldStyleText,),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/2,
-                  child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: AnimatedLineChart(
-                                chart,
-                                key: UniqueKey(),
-                              ), //Unique key to force animations
-                            )),
-                        SizedBox(width: 200, height: 50, child: Text('')),
-                      ]),
+                Neumorphic(
+                  style: NeumorphicStyle(
+                      shape: NeumorphicShape.concave,
+                      boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                      depth: 4,
+                      lightSource: LightSource.topLeft,
+                      color: Apptheme.background
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height/2,
+                      child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: AnimatedLineChart(
+                                    chart,
+                                    key: UniqueKey(),
+                                  ), //Unique key to force animations
+                                )),
+                            SizedBox(width: 200, height: 50, child: Text('')),
+                          ]),
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal:12.0),
